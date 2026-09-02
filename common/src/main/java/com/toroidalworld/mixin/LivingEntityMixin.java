81765d9d82f3c4aa637bd85fee5c3f0b651c152b
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-02 15:33:59 +0300

    refactor(world): fold the landing coordinate handed to checkFallDamage

--- common/src/main/java/com/toroidalworld/mixin/LivingEntityMixin.java
+++ common/src/main/java/com/toroidalworld/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package com.toroidalworld.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -11,6 +12,7 @@ import com.toroidalworld.core.WorldFold;
 import com.toroidalworld.entity.SeamAim;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 
@@ -57,24 +59,14 @@ public class LivingEntityMixin {
             method = "checkFallDamage(DZLnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getX()D"))
     private double toroidal$landingXNearBlock(double x, @Local(argsOnly = true) BlockPos pos) {
-        WorldFold transformer = ((TransformerSource) this).toroidal$wrappedTransformer();
-        if (transformer == null) {
-            return x;
-        }
-
-        return transformer.nearestCopy(Vec3.atCenterOf(pos), ((LivingEntity) (Object) this).position()).x;
+        return toroidal$nearestLandingCoordinate(Direction.Axis.X, x, pos);
     }
 
     @ModifyExpressionValue(
             method = "checkFallDamage(DZLnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getZ()D"))
     private double toroidal$landingZNearBlock(double z, @Local(argsOnly = true) BlockPos pos) {
-        WorldFold transformer = ((TransformerSource) this).toroidal$wrappedTransformer();
-        if (transformer == null) {
-            return z;
-        }
-
-        return transformer.nearestCopy(Vec3.atCenterOf(pos), ((LivingEntity) (Object) this).position()).z;
+        return toroidal$nearestLandingCoordinate(Direction.Axis.Z, z, pos);
     }
 
     @ModifyExpressionValue(
@@ -83,5 +75,15 @@ public class LivingEntityMixin {
     private BlockPos toroidal$landingBlockNearBlock(BlockPos entityPos, @Local(argsOnly = true) BlockPos pos) {
         WorldFold transformer = ((TransformerSource) this).toroidal$wrappedTransformer();
         return transformer == null ? entityPos : transformer.nearestCopy(pos, entityPos);
+    }
+
+    @Unique
+    private double toroidal$nearestLandingCoordinate(Direction.Axis axis, double coordinate, BlockPos landingBlock) {
+        WorldFold transformer = ((TransformerSource) this).toroidal$wrappedTransformer();
+        if (transformer == null) {
+            return coordinate;
+        }
+
+        return transformer.blockDomain(axis).unwrapAround(landingBlock.get(axis) + 0.5, coordinate);
     }
 }
