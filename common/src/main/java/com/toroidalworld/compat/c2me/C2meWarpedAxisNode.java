31746d63e8356b1ed4fff779bbf723f6149d9530
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-06 02:03:11 +0300

    feat(world): carry the horizontal domain warp through the fold

---
+++ common/src/main/java/com/toroidalworld/compat/c2me/C2meWarpedAxisNode.java
@@ -0,0 +1,78 @@
+package com.toroidalworld.compat.c2me;
+
+import java.util.Objects;
+
+import com.toroidalworld.core.WrapDomain;
+import com.ishland.c2me.opts.dfc.common.ast.AstNode;
+import com.ishland.c2me.opts.dfc.common.ast.AstTransformer;
+import com.ishland.c2me.opts.dfc.common.ast.misc.CoordinateNode;
+
+public final class C2meWarpedAxisNode implements AstNode {
+    public final CoordinateNode.Axis axis;
+    public final WrapDomain domain;
+    public final AstNode shift;
+    public final double xzScale;
+
+    public C2meWarpedAxisNode(CoordinateNode.Axis axis, WrapDomain domain, AstNode shift, double xzScale) {
+        this.axis = Objects.requireNonNull(axis);
+        this.domain = Objects.requireNonNull(domain);
+        this.shift = Objects.requireNonNull(shift);
+        this.xzScale = xzScale;
+    }
+
+    @Override
+    public AstNode[] getChildren() {
+        return new AstNode[]{this.shift};
+    }
+
+    @Override
+    public AstNode transform(AstTransformer transformer) {
+        AstNode transformedShift = this.shift.transform(transformer);
+
+        return transformer.transform(transformedShift == this.shift
+                ? this
+                : new C2meWarpedAxisNode(this.axis, this.domain, transformedShift, this.xzScale));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        C2meWarpedAxisNode that = (C2meWarpedAxisNode) o;
+        return this.axis == that.axis
+                && this.domain == that.domain
+                && Double.compare(this.xzScale, that.xzScale) == 0
+                && this.shift.equals(that.shift);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.axis.hashCode();
+        result = 31 * result + System.identityHashCode(this.domain);
+        result = 31 * result + Double.hashCode(this.xzScale);
+        return 31 * result + this.shift.hashCode();
+    }
+
+    @Override
+    public boolean relaxedEquals(AstNode o) {
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        C2meWarpedAxisNode that = (C2meWarpedAxisNode) o;
+        return this.axis == that.axis
+                && this.domain == that.domain
+                && Double.compare(this.xzScale, that.xzScale) == 0
+                && this.shift.relaxedEquals(that.shift);
+    }
+
+    @Override
+    public int relaxedHashCode() {
+        int result = this.axis.hashCode();
+        result = 31 * result + System.identityHashCode(this.domain);
+        result = 31 * result + Double.hashCode(this.xzScale);
+        return 31 * result + this.shift.relaxedHashCode();
+    }
+}
