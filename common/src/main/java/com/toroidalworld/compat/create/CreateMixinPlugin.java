ddb78f7d39c98f5b6e90a343000b3496583dbf77
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-05 20:02:28 +0300

    refactor(world): move the create, sable and aeronautics mixin plugins onto the gate base

--- common/src/main/java/com/toroidalworld/compat/create/CreateMixinPlugin.java
+++ common/src/main/java/com/toroidalworld/compat/create/CreateMixinPlugin.java
@@ -1,17 +1,12 @@
 package com.toroidalworld.compat.create;
 
-import java.util.List;
-import java.util.Set;
-
-import org.objectweb.asm.tree.ClassNode;
 import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 import com.mojang.logging.LogUtils;
+import com.toroidalworld.MixinGatePlugin;
 import com.toroidalworld.compat.sable.SableMod;
 
-public class CreateMixinPlugin implements IMixinConfigPlugin {
+public class CreateMixinPlugin extends MixinGatePlugin {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     private static final String BLUEPRINT_REACH_MIXIN =
@@ -41,27 +36,5 @@ public class CreateMixinPlugin implements IMixinConfigPlugin {
         }
 
         return true;
-    }
-
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
     }
 }
--- common/src/main/java/com/toroidalworld/compat/sable/SableMixinPlugin.java
+++ common/src/main/java/com/toroidalworld/compat/sable/SableMixinPlugin.java
@@ -1,13 +1,8 @@
 package com.toroidalworld.compat.sable;
 
-import java.util.List;
-import java.util.Set;
+import com.toroidalworld.MixinGatePlugin;
 
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
-
-public class SableMixinPlugin implements IMixinConfigPlugin {
+public class SableMixinPlugin extends MixinGatePlugin {
     @Override
     public void onLoad(String mixinPackage) {
         SableMod.present();
@@ -16,27 +11,5 @@ public class SableMixinPlugin implements IMixinConfigPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         return SableMod.present();
-    }
-
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
     }
 }
