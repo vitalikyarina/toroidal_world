5a540fe9ad108c13088b11c0f2805f86818a43d0
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-09 13:56:37 +0300

    refactor(world): move TransformerSourceBindable to compat/c2me and seal the constants classes

--- common/src/main/java/com/toroidalworld/compat/c2me/TransformerSourceBindable.java
+++ common/src/main/java/com/toroidalworld/compat/c2me/TransformerSourceBindable.java
@@ -1,4 +1,6 @@
-package com.toroidalworld.accessors;
+package com.toroidalworld.compat.c2me;
+
+import com.toroidalworld.accessors.TransformerSource;
 
 public interface TransformerSourceBindable {
     default void toroidal$bindTransformerSource(TransformerSource source) {
